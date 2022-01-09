09ef09f5428537fc44f1d468b67cfcd78d02a021
Aveiro746 <95100461+Aveiro746@users.noreply.github.com>
2022-01-08 23:14:49 -0500

    alternating clicks per move function added

--- app.js
+++ app.js
@@ -12,20 +12,45 @@
 // [0,4,8]
 // [2,4,6]
 
-//Get block tiles from Html
+
 const tiles= document.getElementById('gamegrid')
+const resetBtn = document.getElementById('restart')
+const blocks = document.getElementsByClassName('block')
+
 
 //Alternate click events from player 1 to player 2
 
 let playerfirstmove= true
 tiles.addEventListener('click' , (event)=>{
-    if (event.target.className === 'block') {
+    if (event.target.className === 'block' && event.target.innerText === "") {
         event.target.innerHTML = playerfirstmove ? "X" : "O";
         playerfirstmove = !playerfirstmove
     }
-    return;
+ 
 })
 // only one input per tile
+
 //win condition
-//tie event
-//reset board
+let grid = ["" , "" , "" , "" , "" , "" , "" ,"" , ""]
+const winCon = [
+[0,1,2],
+[3,4,5],
+[6,7,8],
+[0,3,6],
+[1,4,7],
+[2,5,8],
+[0,4,8],
+[2,4,6]
+]
+
+// inside of wincon arr we have idx we have to check ,  all characters in the given idx need to be the same, none empty , 
+// function checkresult() {
+//    for (let i= 0; i < blocks.length-1; i++) {
+//        for( let j = 0 ; j < blocks.length-1; i++){
+//            const 
+//        }
+//    }
+// //     if ()
+//    }
+// //tie event
+// checkresult()
